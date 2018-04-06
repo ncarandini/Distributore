c8dd1fbf9ba3f859dc8c8dd9d63ec54a73ae4086
Alessio C <darkalessio@live.it>
2018-04-06 15:38:23 +0200

    Implementate Usecase IDispencer

---
+++ TeamLab.Distributore.ConsoleApp/Models/Interfaces/IDispencer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamLab.Distributore.ConsoleApp.Models.Interfaces
+{
+    /*  Mostra sul display se il prodotto è disponibile e il costo del prodotto
+     *  Mostra sul display il prezzo del prodotto se non ha inserito i soldi
+        Mostra sul display se il credito è insufficiente e dice quanto manca
+        Se il credito è sufficiente PRELEVA PRODOTTO*/
+
+    interface IDispencer
+    {
+    }
+}
