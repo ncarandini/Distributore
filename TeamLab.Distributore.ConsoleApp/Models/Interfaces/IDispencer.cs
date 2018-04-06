ff077150716e51acc9e5ea2dcd95abc5c9af13ba
NikoStrappelli <tacos.93@hotmail.it>
2018-04-06 15:40:28 +0200

    Inserite User  Case

---
+++ TeamLab.Distributore.ConsoleApp/Models/Interfaces/IDispencer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamLab.Distributore.ConsoleApp.Models.Interfaces
+{
+    interface IDispencer
+    {
+    }
+}
+
+
+
+/* Use Case:
+ * 1. credito insufficiente, inserire il credito mancante
+ * 2. slot scelto, credito sufficiente, prodotto preso
+ * 3. 
+ */
