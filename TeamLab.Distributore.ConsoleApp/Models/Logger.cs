fd642c389b0e2f847f0adb262973a62092a9ffb1
ncarandini <n.carandini@outlook.com>
2018-04-09 15:08:58 +0200

    Add Logger

---
+++ TeamLab.Distributore.ConsoleApp/Models/Logger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamLab.Distributore.ConsoleApp.Models
+{
+    public static class Logger
+    {
+        public static void Log(object sender, string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
